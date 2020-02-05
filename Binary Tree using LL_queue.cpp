6a25c555c2a0e4ad0cc16074a5ba4de5e761a12e
nulyaka <49660415+nulyaka@users.noreply.github.com>
2020-02-05 06:39:04 +0100

    Add files via upload

---
+++ Binary Tree using LL_queue.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include "LLqueue_for_BinTree.h"
+
+class BinTreeNode{
+public:
+    int data;
+    BinTreeNode *lChild;
+    BinTreeNode *rChild;
+};
+
+class BinTreeList{
+private:
+    BinTreeNode *root;
+public:
+    BinTreeList() { root = nullptr; }
+    
+    void create();
+    BinTreeNode* getRootPointer();
+    
+    void displayPreOrder(BinTreeNode *currentNode);
+    void displayInOrder(BinTreeNode *currentNode);
+    void displayPostOrder(BinTreeNode *currentNode);
+    
+};
+
+BinTreeNode* BinTreeList::getRootPointer() {
+    
+    BinTreeNode *temp = root;
+    
+    return temp;
+}
+
+void BinTreeList::create() {
+    
+    root = new BinTreeNode;
+    root->lChild = root->rChild = nullptr;
+    
+    std::cout << "Enter a root value: ";
+    std::cin >> root->data;
+    
+    QueueLL<BinTreeNode*> queue;
+    queue.enqueue(root);
+    
+    while ( !queue.isEmpty() ) {
+        
+        int tempData(0);
+        BinTreeNode *currentNode = queue.dequeue(),
+                    *tempNode = nullptr;
+        
+        // creating left child for current node
+        std::cout << "Enter left child value of " << currentNode->data << ": ";
+        std::cin >> tempData;
+        
+        if (tempData != -1) {
+            
+            tempNode = new BinTreeNode;
+            tempNode->data = tempData;
+            tempNode->lChild = tempNode->rChild = nullptr;
+            currentNode->lChild = tempNode;
+            queue.enqueue(tempNode);
+        }
+        
+        // creating right child for current node
+        std::cout << "Enter right child value of " << currentNode->data << ": ";
+        std::cin >> tempData;
+        
+        if (tempData != -1) {
+            
+            tempNode = new BinTreeNode;
+            tempNode->data = tempData;
+            tempNode->lChild = tempNode->rChild = nullptr;
+            currentNode->rChild = tempNode;
+            queue.enqueue(tempNode);
+        }
+    }
+}
+
+void BinTreeList::displayPreOrder(BinTreeNode *currentNode) {
+    
+    if (currentNode != nullptr) {
+        
+        std::cout << currentNode->data << ' ';
+        displayPreOrder(currentNode->lChild);
+        displayPreOrder(currentNode->rChild);
+    }
+}
+
+void BinTreeList::displayInOrder(BinTreeNode *currentNode) {
+    
+    if (currentNode != nullptr) {
+        
+        displayInOrder(currentNode->lChild);
+        std::cout << currentNode->data << ' ';
+        displayInOrder(currentNode->rChild);
+    }
+}
+
+void BinTreeList::displayPostOrder(BinTreeNode *currentNode) {
+    
+    if (currentNode != nullptr) {
+        
+        displayPostOrder(currentNode->lChild);
+        displayPostOrder(currentNode->rChild);
+        std::cout << currentNode->data << ' ';
+    }
+}
+
---
+++ LL_queue.h
@@ -0,0 +1,138 @@
+//
+//  LLqueue_for_BinTree.h
+//  Binary_Tree_using_Queue
+//
+//  Created by Nulyaka on 1/18/20.
+//  Copyright © 2020 Nulyaka. All rights reserved.
+//
+
+#ifndef LLqueue_for_BinTree_h
+#define LLqueue_for_BinTree_h
+
+#include <iostream>
+
+template <class T>
+class QueueNode{
+public:
+    T data;
+    QueueNode *prev;
+    QueueNode *next;
+};
+
+template <class T>
+class QueueLL{
+private:
+    QueueNode<T> *front;
+    QueueNode<T> *rear;
+    int size;
+public:
+    QueueLL() { this->front = nullptr; this->rear = nullptr; }
+    
+    bool isEmpty();
+    void display();
+    
+    void enqueue(const T value);
+    T dequeue();
+    
+    ~QueueLL();
+};
+
+template <class T>
+bool QueueLL<T>::isEmpty() {
+    
+    if (front == nullptr) {
+        return true;
+    }
+    
+    return false;
+}
+
+// Display from rear node to front node
+template <class T>
+void QueueLL<T>::display() {
+    
+    QueueNode<T> *current = rear;
+    
+    if (current == nullptr) {
+        return;
+    }
+    
+    while (current != nullptr) {
+        
+        std::cout << current->data << ' ';
+        current = current->prev;
+    }
+    std::cout << std::endl;
+}
+
+template <class T>
+void QueueLL<T>::enqueue(const T value) {
+    
+    QueueNode<T> *temp = new QueueNode<T>;
+    temp->data = value;
+    temp->prev = nullptr;
+    temp->next = nullptr;
+    
+    if (temp == nullptr) {
+        std::cout << "Stack is full!" << std::endl;
+        return;
+    }
+    
+    if (front == nullptr) {
+        front = temp;
+        rear = temp;
+    }
+    else {
+        temp->prev = rear;
+        rear->next = temp;
+        rear = temp;
+    }
+}
+
+template <class T>
+T QueueLL<T>::dequeue() {
+    
+    T data(nullptr);
+    
+    if ( !isEmpty() ) {
+        
+        data = front->data;
+        
+        QueueNode<T> *temp = front;
+        
+        if (front != rear) {
+            front = front->next;
+            front->prev = nullptr;
+        }
+        else {
+            rear = nullptr;
+            front = nullptr;
+        }
+        
+        delete temp;
+    }
+    
+    return data;
+}
+
+template <class T>
+QueueLL<T>::~QueueLL() {
+    
+    if (isEmpty()) {
+        return;
+    }
+    
+    QueueNode<T> *current = nullptr;
+    
+    while(rear != nullptr){
+        
+        current = rear;
+        rear = rear->prev;
+        rear->next = nullptr;
+        
+        delete current;
+        current = nullptr;
+    }
+}
+
+#endif /* LLqueue_for_BinTree_h */
